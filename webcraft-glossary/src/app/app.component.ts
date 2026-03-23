59a362185d02ab1e8b11255f4a6d43e47818b031
Alex Kraasch <alex@kraasch.eu>
2026-03-23 17:45:18 +0100

    Add header and footer components.

--- webcraft-glossary/src/app/app.component.ts
+++ webcraft-glossary/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyfooterComponent } from './myfooter/myfooter.component';
+import { MyheaderComponent } from './myheader/myheader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MyheaderComponent,
+    MyfooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'webcraft-glossary';
 }
+
---
+++ webcraft-glossary/src/app/myheader/myheader.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myheader',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      myheader works!
+    </p>
+  `,
+  styles: ``
+})
+export class MyheaderComponent {
+
+}
